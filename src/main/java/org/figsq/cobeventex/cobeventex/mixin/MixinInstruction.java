f60c8f84312c30377093e3444002eba00fab1e49
FullIdle <2675423979@qq.com>
2025-06-16 23:43:14 +0800

    简化

---
+++ src/main/java/org/figsq/cobeventex/cobeventex/mixin/MixinInstruction.java
@@ -0,0 +1,86 @@
+package org.figsq.cobeventex.cobeventex.mixin;
+
+import com.cobblemon.mod.common.api.battles.model.PokemonBattle;
+import com.cobblemon.mod.common.battles.interpreter.instructions.*;
+import org.figsq.cobeventex.cobeventex.api.bridge.IInstructionBridge;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = {
+        AbilityInstruction.class,
+        ActivateInstruction.class,
+        BagItemInstruction.class,
+        BlockInstruction.class,
+        BoostInstruction.class,
+        CantInstruction.class,
+        CenterInstruction.class,
+        ClearAllBoostInstruction.class,
+        ClearBoostInstruction.class,
+        ClearNegativeBoostInstruction.class,
+        CopyBoostInstruction.class,
+        CritInstruction.class,
+        CureStatusInstruction.class,
+        DamageInstruction.class,
+        DragInstruction.class,
+        EndAbilityInstruction.class,
+        EndInstruction.class,
+        EndItemInstruction.class,
+        ErrorInstruction.class,
+        FailInstruction.class,
+        FaintInstruction.class,
+        FieldActivateInstruction.class,
+        FieldEndInstruction.class,
+        FieldStartInstruction.class,
+        FormeChangeInstruction.class,
+        HealInstruction.class,
+        HitCountInstruction.class,
+        IgnoredInstruction.class,
+        ImmuneInstruction.class,
+        InitializeInstruction.class,
+        InvertBoostInstruction.class,
+        ItemInstruction.class,
+        MegaInstruction.class,
+        MissInstruction.class,
+        MoveInstruction.class,
+        NothingInstruction.class,
+        PpUpdateInstruction.class,
+        PrepareInstruction.class,
+        RechargeInstruction.class,
+        ReplaceInstruction.class,
+        RequestInstruction.class,
+        ResistedInstruction.class,
+        SetBoostInstruction.class,
+        SetHpInstruction.class,
+        SideEndInstruction.class,
+        SideStartInstruction.class,
+        SingleMoveInstruction.class,
+        SingleTurnInstruction.class,
+        StartInstruction.class,
+        StatusInstruction.class,
+        SuperEffectiveInstruction.class,
+        SwapBoostInstruction.class,
+        SwapInstruction.class,
+        SwapSideConditionsInstruction.class,
+        SwitchInstruction.class,
+        TerastallizeInstruction.class,
+        TransformInstruction.class,
+        TurnInstruction.class,
+        UnknownInstruction.class,
+        UpkeepInstruction.class,
+        WeatherInstruction.class,
+        WinInstruction.class,
+        ZBrokenInstruction.class,
+        ZPowerInstruction.class,
+}, remap = false)
+public abstract class MixinInstruction implements IInstructionBridge {
+    @Inject(
+            method = "invoke",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    public void invoke(PokemonBattle battle, CallbackInfo ci) {
+        call(ci, battle);
+    }
+}
